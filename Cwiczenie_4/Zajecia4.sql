1745383b4c9cf88d0a0b27787cfe3c7acbc460b8
Miovaa <147978077+Miovaa@users.noreply.github.com>
2025-11-15 21:04:50 +0100

    Changed - obiekt2

--- Cwiczenie_4/Zajecia4.sql
+++ Cwiczenie_4/Zajecia4.sql
@@ -20,18 +20,16 @@ VALUES (
 INSERT INTO obiekty (nazwa, geom)
 VALUES (
     'obiekt2',
-    ST_Collect(
-        -- Granica zewnętrzna
-        ST_GeomFromText(
-			'COMPOUNDCURVE(
-				(10 6, 14 6), 
-				CIRCULARSTRING(14 6, 16 4, 14 2), 
-				CIRCULARSTRING(14 2, 12 0, 10 2), 
-				(10 2, 10 6)
-		)'),
-        
-        -- Granica wewnętrzna
-        ST_GeomFromText('CIRCULARSTRING(13 2, 12 3, 11 2, 12 1, 13 2)')
+    ST_GeomFromText(
+        'CURVEPOLYGON(
+            COMPOUNDCURVE(
+                (10 6, 14 6),
+                CIRCULARSTRING(14 6, 16 4, 14 2),
+                CIRCULARSTRING(14 2, 12 0, 10 2),
+                (10 2, 10 6)
+            ),
+            CIRCULARSTRING(13 2, 12 3, 11 2, 12 1, 13 2)
+        )'
     )
 );
 
@@ -114,4 +112,5 @@ FROM
 	obiekty
 WHERE
 	ST_HasArc(geom) = FALSE;
+
 	
